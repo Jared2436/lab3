92a12f1d2f75b93589f81f6bfe41e5d7c5831245
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-03-10 00:10:13 +0800

    fix css

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,7 +1,8 @@
 <!doctype html>
 <html>
 <head>
-<?php echo link_tag('assets/css/style.css'); ?>
+<?php //echo link_tag('assets/css/style.css'); ?>
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/style.css'); ?>">  
 <title>
 CodeIgniter Tutorial
 </title>
